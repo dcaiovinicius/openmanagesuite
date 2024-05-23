9aa7cfdccbedcb1175bcc5d8405d167a17a3eec4
dcaiovinicius <caioviniciusdev@gmail.com>
2024-05-23 18:04:42 -0400

    Renomear função de login para new.

--- controllers/login.py
+++ controllers/login.py
@@ -3,5 +3,5 @@ from flask import Blueprint, jsonify, render_template
 login_controller = Blueprint('login_controller', __name__)
 
 @login_controller.route('/')
-def login():
+def new():
     return render_template('login.html')
